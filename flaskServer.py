0de4e8605e16c3caa59d80843eba9828edcf0343
aaronbi <aaron.bi@slu.edu>
2023-12-10 19:50:43 -0600

    update server config

--- flaskServer.py
+++ flaskServer.py
@@ -44,4 +44,4 @@ if __name__ == '__main__':
     app.secret_key = 'secret key abc'
     app.config['SESSION_TYPE'] = 'filesystem'
 
-    app.run(host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0',port=5000)
